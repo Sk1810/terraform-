5dac78dd6c3836b37d8425d7d83ce4317828222c
Sk1810 <106330998+Sk1810@users.noreply.github.com>
2022-09-28 22:55:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 resource "aws_acm_certificate" "cert" {
+  provider          = aws.us_region
   domain_name       = var.STAGE == "hello" ? var.fe_domain_name : join(".", [var.STAGE, var.fe_domain_name])
   validation_method = "DNS"
-
-  tags = {
-    Environment = "sk"
-  }
+  tags = merge(tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)   
 }    
