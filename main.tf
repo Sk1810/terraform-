9c6baff8688702364dd64ef38f3461d200c367c6
Sk1810 <106330998+Sk1810@users.noreply.github.com>
2022-09-28 23:47:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate" "cert" {
   provider          = aws.us_region
-  domain_name       = "${lookup(var.STAGE, var.fe_domain_name)}"
+  domain_name       = var.fe_domain_name
   validation_method = "DNS"
-  tags = merge(tomap({ "STAGE" : var.STAGE }), var.DEFAULT_TAGS)   
+  tags = var.DEFAULT_TAGS   
 }    
