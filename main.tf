7e6f1811fffc1f2fbe27df730069daf1e6915681
Sk1810 <106330998+Sk1810@users.noreply.github.com>
2022-09-28 22:51:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "cert" {
-  domain_name       = "sk.studyview.getdecode.io"
+  domain_name       = var.STAGE == "hello" ? var.fe_domain_name : join(".", [var.STAGE, var.fe_domain_name])
   validation_method = "DNS"
 
   tags = {
