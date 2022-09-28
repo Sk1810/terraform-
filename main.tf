41b9522b8270ef270f2d06493fb6ccc06b151c79
Sk1810 <106330998+Sk1810@users.noreply.github.com>
2022-09-29 00:03:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,24 @@ resource "aws_acm_certificate" "cert" {
   validation_method = "DNS"
   tags = var.DEFAULT_TAGS   
 }    
+
+resource "aws_s3_bucket" "sk_s3_bucket" {
+  provider = aws.us_region
+  bucket   = var.fe_domain_name
+  tags     = var.DEFAULT_TAGS
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  provider = aws.us_region
+  bucket   = aws_s3_bucket.sk_s3_bucket.id
+  acl      = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  provider                = aws.sea_region
+  bucket                  = aws_s3_bucket.sk_s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
