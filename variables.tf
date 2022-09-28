048256bd32b4f1b1a0aa45ff968a00d6ec70b37d
Sk1810 <106330998+Sk1810@users.noreply.github.com>
2022-09-28 23:14:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,12 @@ variable "fe_domain_name" {
   type = string
   description = "Frontend Domain Name"
 }
+
 variable "STAGE" {
   type = string
   description = "Stage for deployment"
 }
+
 variable "DEFAULT_TAGS" {
   type = map(any)
   description = "Default Tags for all resources"
