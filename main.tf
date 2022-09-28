052dbb083e9844cc99b80fce45ce38e3f68c4138
Sk1810 <106330998+Sk1810@users.noreply.github.com>
2022-09-29 00:05:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_s3_bucket_acl" "s3_bucket_acl" {
 }
 
 resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
-  provider                = aws.sea_region
+  provider                = aws.us_region
   bucket                  = aws_s3_bucket.sk_s3_bucket.id
   block_public_acls       = true
   block_public_policy     = true
