7340bc65c5f54df767c0c2dbd2cc467805e5f359
Sk1810 <106330998+Sk1810@users.noreply.github.com>
2022-09-29 10:28:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
-variable "sk-domain-name" {
+variable "sk-bucket-name" {
   type = string
-  description = "Frontend Domain Name"
+  description = "bucket-name"
 }
 
 variable "DEFAULT_TAGS" {
   type = map(any)
   description = "Default Tags for all resources"
+}
+
+variable "sk-domain-name" {
+  type = string
+  description = "Frontend Domain Name"
 }
