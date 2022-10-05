9bbc21d678926f33644bdc5ee2bd5834330aafd7
Karthick <106330998+Sk1810@users.noreply.github.com>
2022-10-05 18:35:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,17 +3,3 @@ provider "aws" {
     alias = "sea_region"
 }
 
-provider "aws" {
-    region = "ap-south-1"
-    alias = "in_region"
-}
-
-provider "aws" {
-    region = "us-east-1"
-    alias = "us_region"
-}
-
-provider "aws" {
-    region = "eu-north-1"
-    alias = "eu_region"
-}
